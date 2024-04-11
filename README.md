24756830a0971650b8dda6dc3cd3549287363aab
YujiroTakahashi <yujiro@cro-co.co.jp>
2024-04-11 23:24:53 +0900

    .gitignore追加 不要なコード除去

--- README.md
+++ README.md
@@ -11,3 +11,23 @@ git clone https://huggingface.co/sonoisa/t5-qiita-title-generation
 ```
 
 [Hugging Face：sonoisa/t5-qiita-title-generation](https://huggingface.co/sonoisa/t5-qiita-title-generation)
+
+
+## toch jit traceしたモデルの作成
+
+```
+docker-compose up -d
+docker-compose exec shell ./build.py
+
+ls opt/
+build.py  title.py  traced_jit.pt
+```
+
+## toch script のロード
+
+```
+docker-compose up -d
+docker-compose exec shell ./title.py
+
+title: インターネットで"誰か"を幸せにする
+```
--- opt/build.py
+++ opt/build.py
@@ -2,21 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import torch
-from torchtext.models import T5Conf, T5Bundle
+from torchtext.models import T5Bundle
 from torchtext.models.t5.bundler import GenerationUtilsForT5
 from torchtext.prototype.generate import GenerationUtils
 
 model_path = "/opt/model/t5-qiita-title-generation"
 
-# TorchText形式の設定を作成
-t5_config = T5Conf(
-    encoder_only=True,
-    embedding_dim=512,
-    num_attention_heads=12,
-    num_encoder_layers=12,
-    num_decoder_layers=12,
-    ffn_dimension=2048,
-)
 # TorchText形式のモデルを作成
 model = T5Bundle.build_model_from_huggingface_ckpt(model_path)
 
